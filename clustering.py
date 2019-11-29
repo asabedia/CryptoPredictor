739a1e2a2343d0ec404fa3ba8dde65370203252c
Moe <MoeCheikh@users.noreply.github.com>
2019-11-28 22:36:03 -0500

    .

--- clustering.py
+++ clustering.py
@@ -1,9 +1,12 @@
 from sklearn.cluster import AffinityPropagation, KMeans
-import numpy as np 
+import numpy as np
+import iso8601 as iso
 import pandas as pd
+from yellowbrick.cluster import KElbowVisualizer
+from datetime import datetime
 
 df = pd.read_csv('csv/processed/processed_cleaned_BTC.csv')
-df_stripped= df[449:1165]
+df_stripped = df[449:1165]
 df_train = df_stripped.reset_index().drop(columns=['time','index','z_score','price_mean','open','close'])
 
 def affinity():
@@ -14,8 +17,23 @@ def affinity():
         df_clusters = df_clusters.append(df_train.iloc[i])
         df_clusters.loc[i,'time'] = df_stripped.iloc[i]['time']
     return df_clusters
+def elbow_point(df):
+    visualizer = KElbowVisualizer(
+        KMeans(), k=(2, 10), metric='calinski_harabasz', timings=False
+    )
+    visualizer.fit(df)
+    return visualizer.elbow_value_
 
 def km():
-    kmeans = KMeans(n_clusters=10).fit(df_train)
+
+    kmeans = KMeans(n_clusters=elbow_point(df_train)).fit(df_train)
     print(kmeans.cluster_centers_)
-km()
+    return kmeans.labels_
+
+df_output= df_stripped.reset_index().drop(columns=['index','z_score','price_mean','open','close'])
+df_output['Cluster_Assigned'] = km()
+df_output['Hour'] = df_output['time'].apply(lambda x: iso.parse_date(x).hour)
+
+gb = df_output.groupby(['Cluster_Assigned','Hour']).size()
+# print(gb)
+gb.to_csv('csv/group_by_hours.csv')
