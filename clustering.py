3413e4d44f0dac917bae1432f0333c07d6d8c06c
Raja-S117 <55563537+Raja-S117@users.noreply.github.com>
2019-11-28 19:41:16 -0500

    ...

--- clustering.py
+++ clustering.py
@@ -1,15 +1,21 @@
-from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AffinityPropagation, KMeans
 import numpy as np 
 import pandas as pd
 
 df = pd.read_csv('csv/processed/processed_cleaned_BTC.csv')
 df_stripped= df[449:1165]
-df_train = df_stripped.reset_index().drop(columns=['time','index'])
-cluster = AffinityPropagation().fit(df_train)
-df_clusters = pd.DataFrame(columns = [df_train.columns,'time'])
-for i in cluster.cluster_centers_indices_:
-    df_clusters = df_clusters.append(df_train.iloc[i])
-    df_clusters['time'] = append(df_stripped['time'].iloc[i])
+df_train = df_stripped.reset_index().drop(columns=['time','index','z_score','price_mean','open','close'])
 
+def affinity():
+    cluster = AffinityPropagation().fit(df_train)
+    df_clusters = pd.DataFrame(columns = df_train.columns)
+    df_clusters['time'] =  0
+    for i in cluster.cluster_centers_indices_:
+        df_clusters = df_clusters.append(df_train.iloc[i])
+        df_clusters.loc[i,'time'] = df_stripped.iloc[i]['time']
+    return df_clusters
 
-print(df_clusters)
+def km():
+    kmeans = KMeans(n_clusters=10).fit(df_train)
+    print(kmeans.cluster_centers_)
+km()
