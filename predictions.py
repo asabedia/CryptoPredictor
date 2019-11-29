64e34d19bef9fd9f89b22efbb8349e7bcae917ec
moecheikh98 <cheikh98mohammad@gmail.com>
2019-11-29 14:25:54 -0500

    .

--- predictions.py
+++ predictions.py
@@ -1,34 +1,56 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import ensemble
 import sklearn.ensemble
 from sklearn import datasets
 from sklearn.utils import shuffle
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv('csv/processed/BTC_with_news.csv')
 df['output'] = df['close'].shift(1)
-df = df.drop(columns=['time','Day'])
+df = df.drop(columns=['time','Day','price_mean'])
 df = df.drop(0)
 df = df.dropna()
 
+
+
+def random_forest():
+    df_new = pd.DataFrame(columns =['predicted','actual'])
+    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns=['output']), df['output'], test_size=0.2,random_state = 30)
+    rfr = sklearn.ensemble.RandomForestRegressor()
+    rfr.fit(x_train, y_train)
+    df_new['predicted'] = rfr.predict(x_test)
+    df_new['actual'] = y_test.values
+    print(mean_squared_error(df_new['actual'],df_new['predicted']))
+
 def gradient_boosting():
     df_new = pd.DataFrame(columns =['predicted','actual'])
-    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns=['output']), df['output'], test_size=0.2,random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns=['output']), df['output'], test_size=0.2,random_state = 30)
     xgb = sklearn.ensemble.GradientBoostingRegressor()
     xgb.fit(x_train, y_train)
-    # print("Gradient Boosting Accuracy: ", xgb.score(x_test, y_test))
     df_new['predicted'] = xgb.predict(x_test)
     df_new['actual'] = y_test.values
-    # print(y_test.values)
-    print(df_new)
+    print(mean_squared_error(df_new['actual'],df_new['predicted']))
 
+def linear_regression():
+    df_new = pd.DataFrame(columns =['predicted','actual'])
+    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns=['output']), df['output'], test_size=0.2,random_state = 30)
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    df_new['predicted'] = lr.predict(x_test)
+    df_new['actual'] = y_test.values
+    print(mean_squared_error(df_new['actual'],df_new['predicted']))
 
+# print(df.corr().abs())
+#
+# df.corr().abs().to_csv('csv/correlation.csv')
 
-
-
+print("Gradient Boosting MSE:")
 gradient_boosting()
-
+print("Random Forest MSE:" )
+random_forest()
+print("Linear Regression MSE:" )
+linear_regression()
 # print(df)
