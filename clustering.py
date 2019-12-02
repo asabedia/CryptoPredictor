ca198e7412964e01930cf36e85734f786a0c20f5
Moe <MoeCheikh@users.noreply.github.com>
2019-12-01 23:55:19 -0500

    ....

--- clustering.py
+++ clustering.py
@@ -22,12 +22,26 @@ def elbow_point(df):
 
 
 kmeans = KMeans(n_clusters=elbow_point(df_train)).fit(df_train)
+
+## SCATTER PLOT CLUSTER ##
 plt.scatter(df_train['price_mean'], df_train['price_stdev'], c=kmeans.predict(df_train), s=50, cmap='viridis')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 plt.xlabel('mean')
 plt.ylabel('stdev')
 plt.show()
+plt.close()
 
-# df_stripped['cluster']=kmeans.labels_
+## SCATTER PLOT TIME CLUSTER ##
+df_time = df_stripped.iloc[::-1]
+df_time['cluster']=kmeans.labels_
+plt.scatter(df_time['Day'], df_time['cluster'], c=kmeans.predict(df_train), s=50, cmap='viridis')
+plt.xlabel('time')
+plt.ylabel('cluster')
+plt.xticks(df_time['Day'][::14], rotation='vertical')
+plt.show()
+plt.close()
+
 # df_stripped.to_csv('cluster_news_delta.csv')
 # df_output= df_stripped.reset_index().drop(columns=['index','z_score','price_mean','open','close'])
 
--- pred.py
+++ pred.py
@@ -41,7 +41,8 @@ def split_dataset(data, test_percent: int):
 
 type_hourly ='csv/processed/BTC_with_news.csv'
 type_daily = 'csv/processed/BTC_with_news_daily.csv'
-df = pd.read_csv(type_daily)
+df = pd.read_csv(type_hourly)
+df2 = df.copy()
 
 ## GRAPH ENTIRE DATASET ##
 df_rev = df.iloc[::-1]
@@ -50,11 +51,12 @@ plt.plot(df_rev['Day'], df_rev['price_mean'])
 plt.title('BTC Price Over Time ')
 figure.savefig('graphs/BTC_Price_Over_Time.png',bbox_inches = "tight")
 plt.close()
+
 ## POST PROCESSING ##
 df['output'] = df['high'].shift(1)
 df = df.drop(0)
 df = df.dropna()
-df = df.drop(columns=['time', 'Day','open','z_score'])
+df = df.drop(columns=['time', 'Day','open','z_score','low','high','price_mean'])
 
 
 ## CORRELATION TESTS ##
@@ -75,8 +77,8 @@ lr = linear_regression(X_train, Y_train)
 ## OUTPUT ##
 
 #LinReg
-pdf = pd.DataFrame(columns=['pred', 'true'])
-pdf['pred'] = lr.predict(X_test)
+pdf = pd.DataFrame(columns=['true'])
+pdf['pred_Lin_Reg'] = lr.predict(X_test)
 pdf['true'] = Y_test.to_numpy()
 print("LinReg RMSE: ", np.sqrt(mean_squared_error(Y_test, lr.predict(X_test))))
 print(pdf)
@@ -84,11 +86,16 @@ print(pdf)
 #Xgboost
 pdf2 = pd.DataFrame(columns=['pred', 'true'])
 pdf2['pred'] = xgbm.predict(X_test)
+pdf['pred_XGBoost'] = xgbm.predict(X_test)
 pdf2['true'] = Y_test.to_numpy()
 print("XGB RMSE: ", np.sqrt(mean_squared_error(Y_test, xgbm.predict(X_test))))
-print (pdf2)
+print(pdf2)
 
-## FEATURE IMPORTANCE ##
+## FEATURE IMPORTANCE Linear Regression##
+f_imp_lr = df.corr()
+print(f_imp_lr['output'])
+
+## FEATURE IMPORTANCE XGBoost##
 feat_imp = pd.Series(xgbm.feature_importances_,index=X_train.transpose().index).sort_values(ascending=False)
 feature_importance = plt.figure(figsize = (6,6))
 sns.barplot(x=feat_imp, y=feat_imp.index)
@@ -99,3 +106,20 @@ plt.legend()
 feat_imp.to_frame().rename(columns={0: "Feature Importance"}).to_html('graphs/Feature_Importance.html')
 feature_importance.savefig("graphs/XGB_Feature_Importance.png", bbox_inches = "tight")
 plt.close()
+
+## Predictions ##
+test, train = split_dataset(df2, 20)
+pdf['time'] = test['time']
+figure = plt.figure(figsize = (12,6))
+pdf = pdf.iloc[::-1]
+plt.plot( 'time', 'pred_Lin_Reg', data=pdf, marker='',  color='red', linewidth=1, label='Linear Regression')
+plt.plot( 'time', 'pred_XGBoost', data=pdf, marker='', color='green', linewidth=1,label="XGBoost")
+plt.plot( 'time', 'true', data=pdf, marker='', color='black', linewidth=2,label="Actual",alpha=0.5)
+plt.xticks(pdf['time'][::336], rotation='vertical')
+plt.title('Hourly Predictions')
+plt.legend()
+figure.savefig("graphs/predictions.png", bbox_inches = "tight")
+
+plt.close()
+
+
